8788122f891567007561252a80ca19e9c1b2eacd
Ringo Leese <r.leese@locr.com>
2025-02-07 09:21:59 +0100

    executed tsc

--- src/js/Vms2TileDbReader.d.ts
+++ src/js/Vms2TileDbReader.d.ts
@@ -3,6 +3,16 @@ declare class SQLite {
     #private;
     constructor(filename: string);
     /**
+     * Get the specified data from the database. \
+     * This method can throw an exception, if the $type-parameter is invalid or if the internal database query failed.
+     *
+     * ```js
+     * import { SQLite } from '@locr-company/vms2-tile-db-reader';
+     *
+     * const tileDb = new SQLite('data/world.sqlite');
+     * const tileData = tileDb.getRawData(34686, 21566, 16, 'building', '*', 'polygons');
+     * ```
+     *
      * @returns {Buffer}
      */
     getRawData(x: number, y: number, z: number, key: string, value?: string, type?: Type): Buffer<ArrayBuffer>;
